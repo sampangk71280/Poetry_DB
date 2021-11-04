97e947a033a33c654f89b93c20ef6ef5c4eedd8e
sampangk71280 <51727247+sampangk71280@users.noreply.github.com>
2021-11-05 11:01:01 +1300

    recent displayed but no authors or subjects

--- content/recent.php
+++ content/recent.php
@@ -2,7 +2,7 @@
 
 <?php 
 
-$find_sql = "SELECT * FROM `poetry` JOIN `author` ON(`author`.`Author_ID` = `poetry`.`Author_ID`) ORDER BY `ID` DESC";
+$find_sql = "SELECT * FROM `poetry` JOIN `author` ON(`author`.`Author_ID` = `poetry`.`Author_ID`) ORDER BY `ID` DESC LIMIT 5";
 
 $find_query = mysqli_query($dbconnect, $find_sql);
 $find_rs = mysqli_fetch_assoc($find_query);
@@ -10,18 +10,19 @@ $find_rs = mysqli_fetch_assoc($find_query);
 // loop through results and display them... 
 do {
 
-    $quote = preg_replace('/[^A-Za-z0-9.,?\s\'\-]/', ' ', $find_rs['context']);
+    $title = preg_replace('/[^A-Za-z0-9.,?\s\'\-]/', ' ', $find_rs['Title']);
+    $content = preg_replace('/[^A-Za-z0-9.,?\s\'\-]/', ' ', $find_rs['Content']);
 
     // get author name
-    include("get_author.php");
+    // include("get_author.php");
 
     ?>
 <div class="results">
     <p>
-        <?php echo $quote; ?><br />
-        <a href="index.php?page=author&authorID=<?php echo $find_rs['Author_ID'];
-        ?>">
-            <?php echo $full_name; ?>
+        <?php echo $title; ?><br/> <br />
+        <?php echo $content; ?><br />
+        <!-- <a href="index.php?page=author&authorID=<?php echo $find_rs['Author_ID'];?>"> -->
+            <!-- <?php echo $full_name; ?> -->
         </a>
     </p>
 
